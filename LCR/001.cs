58bc7c6e903e8573571b423fcc6aeab5a8923fbe
TengoDango <1353878448@qq.com>
2024-05-17 15:00:11 +0800

    添加项目文件。

---
+++ LCR/001.cs
@@ -0,0 +1,16 @@
+﻿namespace Leetcode.LCR001;
+
+// https://leetcode.cn/problems/xoh6Oh/?envType=study-plan-v2&envId=coding-interviews-special
+
+public class Solution
+{
+    /// <summary>
+    /// 不使用 * / %，求商 a / b；
+    /// 
+    /// 如果除法结果溢出，则返回 2^31 − 1
+    /// </summary>
+    public int Divide(int a, int b)
+    {
+        return a / b;
+    }
+}
